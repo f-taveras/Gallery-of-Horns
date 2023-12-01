d59d32f13020aa03aebd4c0df79bc3ffaddaa541
Felix A. Taveras <felixataveras1@gmail.com>
2023-12-01 05:06:10 -0500

    all features finished

--- src/components/Gallery/HornedBeast.jsx
+++ src/components/Gallery/HornedBeast.jsx
@@ -1,20 +1,67 @@
-import React from 'react';
-import Gallery from './Gallery';
+import { useState } from "react";
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Modal from "react-bootstrap/Modal";
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Heart from 'react-animated-heart';
+import '../../Gallery.css';
 
 
+function HornedBeast({ name, image_url, description }) {
 
-function HornedBeast(props){
-  return(
-<>
-{
-  props.list.map((animal,indx) => {
- return <Gallery key={indx} name={animal.title} description={animal.description} img={animal.image_url}/>
-  
-  })
+  const [isClick, setClick] = useState(false);
+  const [likes, setLikes] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+
+  function handleFavorite() {
+    if (!isClick) {
+      setLikes(likes + 1);
+    }
+    setClick(!isClick);
+  }
+
+  const handleModalShow = () => setShowModal(true);
+  const handleModalClose = () => setShowModal(false);
+
+  return (
+
+    <Container>
+      <Row>
+        <Col className="gallery-col">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={image_url} alt={name} onClick={handleModalShow} />
+            <Card.Body>
+              <Card.Title>{name}</Card.Title>
+              <Card.Text>{description}</Card.Text>
+              <div className="heart-container">
+                <Heart isClick={isClick} onClick={handleFavorite} />
+                <Card.Text className="likes">Favorited: {likes}</Card.Text>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img src={image_url} alt={name} style={{ width: '100%' }} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleModalClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+
+    </Container>
+  );
 }
-</>
-  )
-}
-
 
 export default HornedBeast;
